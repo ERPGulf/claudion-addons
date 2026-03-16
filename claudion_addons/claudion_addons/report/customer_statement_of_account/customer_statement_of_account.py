1effea1dfd0e90273081f3e47d4b3492a053dd26
Malavika G <harsha@htsqatar.com>
2026-03-16 10:42:18 +0300

    added prnt format for customer statement of account report

--- claudion_addons/claudion_addons/report/customer_statement_of_account/customer_statement_of_account.py
+++ claudion_addons/claudion_addons/report/customer_statement_of_account/customer_statement_of_account.py
@@ -1,154 +1,4 @@
-# # # Copyright (c) 2026, ERPGulf and contributors
-# # # For license information, please see license.txt
 
-# # # import frappe
-
-
-# # def execute(filters=None):
-# # 	columns, data = [], []
-# # 	return columns, data
-
-# # import frappe
-# # from frappe import _
-# # from erpnext.accounts.report.general_ledger import general_ledger
-
-# # """
-# # Customer Statement of Account Report
-# # Based on General Ledger report filtered for Customer
-# # """
-
-
-# # def execute(filters=None):
-# # 	if not filters:
-# # 		return [], []
-
-# # 	filters = frappe._dict(filters)
-# # 	filters.party_type = "Customer"
-
-# # 	# Normalize party filter
-# # 	if filters.get("party") and not isinstance(filters.party, (list, tuple)):
-# # 		filters.party = [filters.party]
-
-# # 	# --------------------------------------------------
-# # 	# IGNORE SYSTEM GENERATED CR / DR NOTES
-# # 	# --------------------------------------------------
-# # 	if filters.get("ignore_cr_dr_notes"):
-# # 		system_generated_cr_dr_journals = frappe.db.get_all(
-# # 			"Journal Entry",
-# # 			filters={
-# # 				"company": filters.get("company"),
-# # 				"docstatus": 1,
-# # 				"voucher_type": ("in", ["Credit Note", "Debit Note"]),
-# # 				"is_system_generated": 1,
-# # 			},
-# # 			as_list=True,
-# # 		)
-
-# # 		if system_generated_cr_dr_journals:
-# # 			vouchers_to_ignore = (filters.get("voucher_no_not_in") or []) + [
-# # 				x[0] for x in system_generated_cr_dr_journals
-# # 			]
-# # 			filters.update({"voucher_no_not_in": vouchers_to_ignore})
-
-# # 	# --------------------------------------------------
-# # 	# GET GENERAL LEDGER DATA
-# # 	# --------------------------------------------------
-# # 	columns, data = general_ledger.execute(filters)
-
-# # 	# --------------------------------------------------
-# # 	# ADD CUSTOMER NAME COLUMN IF MISSING
-# # 	# --------------------------------------------------
-# # 	if not any(col.get("fieldname") == "party_name" for col in columns):
-# # 		party_index = next(
-# # 			i for i, c in enumerate(columns) if c.get("fieldname") == "party"
-# # 		)
-# # 		columns.insert(
-# # 			party_index + 1,
-# # 			{
-# # 				"label": _("Customer Name"),
-# # 				"fieldname": "party_name",
-# # 				"fieldtype": "Data",
-# # 				"width": 150,
-# # 			},
-# # 		)
-
-# # 	# --------------------------------------------------
-# # 	# ADD REMARKS COLUMN (ONLY IF FILTER ENABLED)
-# # 	# --------------------------------------------------
-# # 	if filters.get("show_remarks"):
-# # 		columns.append(
-# # 			{
-# # 				"label": _("Remarks"),
-# # 				"fieldname": "remarks",
-# # 				"fieldtype": "Data",
-# # 				"width": 400,
-# # 			}
-# # 		)
-
-# # 	# --------------------------------------------------
-# # 	# ENRICH DATA
-# # 	# --------------------------------------------------
-# # 	customer_map = get_customer_map()
-
-# # 	for row in data:
-# # 		if row.get("party") and row.get("party_type") == "Customer":
-# # 			row["party_name"] = customer_map.get(row["party"], "")
-
-# # 		# Attach remarks only if enabled
-# # 		if filters.get("show_remarks"):
-# # 			remarks_length = frappe.get_single_value(
-# # 				"Accounts Settings", "general_ledger_remarks_length"
-# # 			)
-
-# # 			if remarks_length and row.get("remarks"):
-# # 				row["remarks"] = row.get("remarks")[:remarks_length]
-
-# # 	return columns, data, None, get_report_summary(filters)
-
-
-# # # --------------------------------------------------
-# # # REPORT SUMMARY
-# # # --------------------------------------------------
-# # def get_report_summary(filters):
-# # 	customer_name, tax_id = "", ""
-
-# # 	if filters.get("party"):
-# # 		customer = frappe.db.get_value(
-# # 			"Customer",
-# # 			filters.party[0],
-# # 			["customer_name", "tax_id"],
-# # 			as_dict=True,
-# # 		)
-
-# # 		if customer:
-# # 			customer_name = customer.customer_name
-# # 			tax_id = customer.tax_id
-
-# # 	heading = "<h2 style='text-align:center;'>Statement of Account</h2>"
-# # 	subheading = f"""
-# # 	<div style='text-align:center;'>
-# # 	{customer_name}<br>
-# # 	Tax ID: {tax_id}<br>
-# # 	{filters.from_date} to {filters.to_date}
-# # 	</div>
-# # 	"""
-
-# # 	return [
-# # 		{
-# # 			"value": heading + subheading,
-# # 			"indicator": "blue",
-# # 			"label": "",
-# # 			"datatype": "HTML",
-# # 		}
-# # 	]
-
-
-# # # ---------------------------------- ---- ------------
-# # # CUSTOMER MAP
-# # # --------------------------------------------------
-# # def get_customer_map():
-# # 	customers = frappe.get_all("Customer", fields=["name", "customer_name"])
-# # 	return {c.name: c.customer_name for c in customers}
 # import frappe
 # from frappe import _
 # from erpnext.accounts.report.general_ledger import general_ledger
@@ -158,8 +8,8 @@
 # Based on General Ledger report filtered for Customer
 # Adds:
 #     - Customer Name column
-#     - Description from Sales Invoice (custom_description_)
 #     - Remarks shown only if checkbox enabled
+#     - Customer PO No and PO Date from Sales Invoice
 # """
 
 
@@ -224,25 +74,12 @@
 # 		)
 
 # 	# --------------------------------------------------
-# 	# ADD DESCRIPTION COLUMN
-# 	# --------------------------------------------------
-# 	if not any(col.get("fieldname") == "description" for col in columns):
-# 		columns.append(
-# 			{
-# 				"label": _("Description"),
-# 				"fieldname": "description",
-# 				"fieldtype": "Data",
-# 				"width": 300,
-# 			}
-# 		)
-
-# 	# --------------------------------------------------
 # 	# PREPARE LOOKUP MAPS
 # 	# --------------------------------------------------
 # 	customer_map = get_customer_map()
-# 	sales_invoice_description_map = get_sales_invoice_description_map(data)
+# 	po_map = get_sales_invoice_po_map(data)
 
-# 	# Get remarks length once (performance safe)
+# 	# Get remarks length once
 # 	remarks_length = None
 # 	if filters.get("show_remarks"):
 # 		remarks_length = frappe.get_single_value(
@@ -258,15 +95,14 @@
 # 		if row.get("party") and row.get("party_type") == "Customer":
 # 			row["party_name"] = customer_map.get(row["party"], "")
 
-# 		# Add Description from Sales Invoice
+# 		# Add PO Details
 # 		if row.get("voucher_type") == "Sales Invoice":
-# 			row["description"] = sales_invoice_description_map.get(
-# 				row.get("voucher_no"), ""
-# 			)
-# 		else:
-# 			row["description"] = ""
+# 			po = po_map.get(row.get("voucher_no"))
+# 			if po:
+# 				row["po_no"] = po.get("po_no")
+# 				row["po_date"] = po.get("po_date")
 
-# 		# Handle Remarks (only if checkbox enabled)
+# 		# Handle Remarks
 # 		if filters.get("show_remarks"):
 # 			if remarks_length and row.get("remarks"):
 # 				row["remarks"] = row.get("remarks")[:remarks_length]
@@ -322,30 +158,29 @@
 
 
 # # --------------------------------------------------
-# # SALES INVOICE DESCRIPTION MAP
-# # ---------------------------------------------- ----
-# def get_sales_invoice_description_map(data):
-# 	sales_invoice_numbers = list(
-# 		{
-# 			row.get("voucher_no")
-# 			for row in data
-# 			if row.get("voucher_type") == "Sales Invoice"
-# 			and row.get("voucher_no")
-# 		}
-# 	)
+# # SALES INVOICE PO MAP
+# # --------------------------------------------------
+# def get_sales_invoice_po_map(data):
 
-# 	if not sales_invoice_numbers:
+# 	vouchers = [
+# 		d.get("voucher_no")
+# 		for d in data
+# 		if d.get("voucher_type") == "Sales Invoice"
+# 	]
+
+# 	if not vouchers:
 # 		return {}
 
 # 	invoices = frappe.get_all(
 # 		"Sales Invoice",
-# 		filters={"name": ("in", sales_invoice_numbers)},
-# 		fields=["name", "custom_description_"],
+# 		filters={"name": ["in", vouchers]},
+# 		fields=["name", "po_no", "po_date"],
 # 	)
 
-# 	return {inv.name: inv.custom_description_ for inv in invoices}
+# 	return {inv.name: inv for inv in invoices}
 import frappe
 from frappe import _
+from frappe.utils import formatdate
 from erpnext.accounts.report.general_ledger import general_ledger
 
 """
@@ -354,6 +189,8 @@ Based on General Ledger report filtered for Customer
 Adds:
     - Customer Name column
     - Remarks shown only if checkbox enabled
+    - Customer PO No and PO Date from Sales Invoice
+    - Payment Entry reference_no and reference_date
 """
 
 
@@ -421,6 +258,8 @@ def execute(filters=None):
 	# PREPARE LOOKUP MAPS
 	# --------------------------------------------------
 	customer_map = get_customer_map()
+	po_map = get_sales_invoice_po_map(data)
+	payment_ref_map = get_payment_entry_reference_map(data)
 
 	# Get remarks length once
 	remarks_length = None
@@ -437,6 +276,20 @@ def execute(filters=None):
 		# Add Customer Name
 		if row.get("party") and row.get("party_type") == "Customer":
 			row["party_name"] = customer_map.get(row["party"], "")
+
+		# Add PO Details
+		if row.get("voucher_type") == "Sales Invoice":
+			po = po_map.get(row.get("voucher_no"))
+			if po:
+				row["po_no"] = po.get("po_no")
+				row["po_date"] = po.get("po_date")
+
+		# Add Payment Entry Reference
+		if row.get("voucher_type") == "Payment Entry":
+			ref = payment_ref_map.get(row.get("voucher_no"))
+			if ref:
+				row["reference_no"] = ref.get("reference_no")
+				row["reference_date"] = formatdate(ref.get("reference_date"))
 
 		# Handle Remarks
 		if filters.get("show_remarks"):
@@ -491,3 +344,49 @@ def get_report_summary(filters):
 def get_customer_map():
 	customers = frappe.get_all("Customer", fields=["name", "customer_name"])
 	return {c.name: c.customer_name for c in customers}
+
+
+# --------------------------------------------------
+# SALES INVOICE PO MAP
+# --------------------------------------------------
+def get_sales_invoice_po_map(data):
+
+	vouchers = [
+		d.get("voucher_no")
+		for d in data
+		if d.get("voucher_type") == "Sales Invoice"
+	]
+
+	if not vouchers:
+		return {}
+
+	invoices = frappe.get_all(
+		"Sales Invoice",
+		filters={"name": ["in", vouchers]},
+		fields=["name", "po_no", "po_date"],
+	)
+
+	return {inv.name: inv for inv in invoices}
+
+
+# --------------------------------------------------
+# PAYMENT ENTRY REFERENCE MAP
+# --------------------------------------------------
+def get_payment_entry_reference_map(data):
+
+	vouchers = [
+		d.get("voucher_no")
+		for d in data
+		if d.get("voucher_type") == "Payment Entry"
+	]
+
+	if not vouchers:
+		return {}
+
+	payments = frappe.get_all(
+		"Payment Entry",
+		filters={"name": ["in", vouchers]},
+		fields=["name", "reference_no", "reference_date"],
+	)
+
+	return {p.name: p for p in payments}
